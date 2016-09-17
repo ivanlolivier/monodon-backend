a71144483be2db82a7c3e249517458334e33b79a
German Larrosa <glarrosa@overactiveinc.com>
2016-09-17 14:00:42 -0300

    Changes on database migration files.

--- database/migrations/2016_09_10_142920_create_visits_table.php
+++ database/migrations/2016_09_10_142920_create_visits_table.php
@@ -19,17 +19,17 @@ class CreateVisitsTable extends Migration
             $table->unsignedInteger('patient_id');
             $table->unsignedInteger('dentist_id');
             $table->unsignedInteger('clinic_id');
-            $table->unsignedInteger('diagnostic_id')->nullable();
+            $table->unsignedInteger('diagnosis_id')->nullable();
             $table->unsignedInteger('treatment_id')->nullable();
             $table->unsignedInteger('parent_visit_id')->nullable();
 
-            $table->enum('type', ['new_reason', 'follow_up_treatment', 'follow_up_diagnostic']);
+            $table->enum('type', ['new_reason', 'follow_up_treatment', 'follow_up_diagnosis']);
 
             //References
             $table->foreign('patient_id')->references('id')->on('patients');
             $table->foreign('dentist_id')->references('id')->on('dentists');
             $table->foreign('clinic_id')->references('id')->on('clinics');
-            $table->foreign('diagnostic_id')->references('id')->on('diagnostics');
+            $table->foreign('diagnosis_id')->references('id')->on('diagnosis');
             $table->foreign('treatment_id')->references('id')->on('treatments');
             $table->foreign('parent_visit_id')->references('id')->on('visits');
 
