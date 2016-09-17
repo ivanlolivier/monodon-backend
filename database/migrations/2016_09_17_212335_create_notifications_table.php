47fb9f856fd0ad289c5c5f799ef1bf37921c2ff2
Ivan L'olivier <ivan.lolivier@gmail.com>
2016-09-17 18:58:36 -0300

    Models and migrations

---
+++ database/migrations/2016_09_17_212335_create_notifications_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('patient_id');
+            $table->foreign('patient_id')->references('id')->on('patients');
+
+            $table->string('title');
+            $table->text('message');
+
+            $table->timestamp('send_at');
+            $table->timestamp('sent_at')->nullable();
+            $table->timestamp('read_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notifications');
+    }
+}
---
+++ database/migrations/2016_09_17_214856_create_alerts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alerts', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('dentist_id');
+            $table->foreign('dentist_id')->references('id')->on('dentists');
+
+            $table->enum('severity', ['important', 'info']);
+            $table->string('title');
+            $table->text('message');
+
+            $table->timestamp('read_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('alerts');
+    }
+}
