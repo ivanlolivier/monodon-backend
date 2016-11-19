7b88a2f821e6d08326d43fb9181efa6a684ecd76
Ivan L'olivier <ivan.lolivier@gmail.com>
2016-11-19 20:08:07 -0300

    Adding CORS

--- routes/api.php
+++ routes/api.php
@@ -4,29 +4,33 @@ use App\Http\Controllers\EmployeeTypeController;
 use LaravelFCM\Facades\FCM;
 use LaravelFCM\Message\PayloadNotificationBuilder;
 
-Route::group(['prefix' => '/clinics'], function () {
-    require 'api/clinics.php';
-});
+Route::group(['middleware' => \App\Http\Middleware\CORS::class], function () {
 
-Route::group(['prefix' => '/patients'], function(){
-    require 'api/patients.php';
-});
+    Route::group(['prefix' => '/clinics'], function () {
+        require 'api/clinics.php';
+    });
 
-Route::group(['prefix' => '/employees'], function(){
-    require 'api/employees.php';
-});
+    Route::group(['prefix' => '/patients'], function () {
+        require 'api/patients.php';
+    });
 
-Route::get('employee_types', EmployeeTypeController::class . '@index');
+    Route::group(['prefix' => '/employees'], function () {
+        require 'api/employees.php';
+    });
 
-Route::get('push', function(){
-    $notification = (new PayloadNotificationBuilder('Titulo de la notificacion'))
-        ->setBody('Hola, soy el cuerpo de la notificación')
-        ->setSound('default')
-        ->build();
+    Route::get('employee_types', EmployeeTypeController::class . '@index');
 
-    $token = "eZviev6PkkM:APA91bHP-cKhWelKXO3LXQb4T3Oa8Fw1a2yz8pHdpWhUadei2xmTFfnifNpuQR1uyXjpuW7eAC8k2NFmcXGriNl1MnayBogdlZ_ETAET9hO8429YKceZIurVCg8T7K-s8R2VPzgo7d3z";
+    Route::get('push', function () {
+        $notification = (new PayloadNotificationBuilder('Titulo de la notificacion'))
+            ->setBody('Hola, soy el cuerpo de la notificación')
+            ->setSound('default')
+            ->build();
 
-    $downstreamResponse = FCM::sendTo($token, null, $notification);
+        $token = "eZviev6PkkM:APA91bHP-cKhWelKXO3LXQb4T3Oa8Fw1a2yz8pHdpWhUadei2xmTFfnifNpuQR1uyXjpuW7eAC8k2NFmcXGriNl1MnayBogdlZ_ETAET9hO8429YKceZIurVCg8T7K-s8R2VPzgo7d3z";
 
-    dd($downstreamResponse);
+        $downstreamResponse = FCM::sendTo($token, null, $notification);
+
+        dd($downstreamResponse);
+    });
+
 });
