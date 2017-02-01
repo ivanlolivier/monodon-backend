4f7b65b3798cf527ec85060d50d44ff811fca4d4
Ivan L'olivier <ivan@decemberlabs.com>
2017-02-01 19:47:56 -0300

    Fix mapping fields in notification sent transformer

--- app/Transformers/NotificationSentTransformer.php
+++ app/Transformers/NotificationSentTransformer.php
@@ -11,8 +11,8 @@ class NotificationSentTransformer extends Transformer
         $this->output = [
             'id'               => $model->id,
             'title'            => $model->scheduled->title,
-            'message'          => $model->scheduled->title,
-            'possible_answers' => $model->scheduled->title,
+            'message'          => $model->scheduled->message,
+            'possible_answers' => $model->scheduled->possible_answers,
             'sent_at'          => $model->sent_at ? $model->sent_at->diffForHumans() : null,
             'read_at'          => $model->read_at ? $model->read_at->diffForHumans() : null,
             'answered_at'      => $model->answered_at ? $model->answered_at->diffForHumans() : null,
