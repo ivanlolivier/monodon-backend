717d5e7904c3174cb4752ff60303dae3301a07b4
Ivan L'olivier <ivan.lolivier@gmail.com>
2017-04-08 16:52:21 -0300

    Invitations for dentists

--- resources/views/email/common.blade.php
+++ resources/views/email/common.blade.php
@@ -17,25 +17,25 @@
                 <tr>
                     <td align="center">
                         <?php
-//                        switch ($button->level) {
-//                            case 'success':
-//                                $actionColor = '#22BC66';
-//                                break;
-//                            case 'error':
-//                                $actionColor = '#dc4d2f';
-//                                break;
-//                            default:
+                        switch ($button['level']) {
+                            case 'success':
+                                $actionColor = '#22BC66';
+                                break;
+                            case 'error':
+                                $actionColor = '#dc4d2f';
+                                break;
+                            default:
                                 $actionColor = '#3869D4';
-//                        }
+                        }
                         ?>
 
-                        <a href="{{ $button->url }}"
+                        <a href="{{ $button['url'] }}"
                            style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; display: block; display: inline-block; width: 200px; min-height: 20px; padding: 10px;
                                    border-radius: 3px; color: #ffffff; font-size: 15px; line-height: 25px; text-align: center; text-decoration: none;
                                    -webkit-text-size-adjust: none; background-color: {{$actionColor}};"
                            class="button"
                            target="_blank">
-                            {{ $button->label }}
+                            {{ $button['label'] }}
                         </a>
                     </td>
                 </tr>
