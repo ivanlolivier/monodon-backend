b01ab48455ed6ea1c4b570b7f588f9b23f4a45d4
Ivan L'olivier <ivan.lolivier@gmail.com>
2018-06-09 15:59:04 -0300

    Adding missing slash

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -29,7 +29,7 @@ class ResetPasswordNotification extends Notification
     {
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', env('FRONTEND_BASE_URL') . '/password/reset' . $this->email . '/' . $this->token)
+            ->action('Reset Password', env('FRONTEND_BASE_URL') . '/password/reset/' . $this->email . '/' . $this->token)
             ->line('If you did not request a password reset, no further action is required.');
     }
 }
