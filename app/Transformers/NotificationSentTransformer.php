efde78ff50bbd162039b77db1a230c3c4a961479
Ivan L'olivier <ilolivier@codigodelsur.com>
2018-07-14 17:59:29 -0300

    fixing dates

--- app/Transformers/NotificationSentTransformer.php
+++ app/Transformers/NotificationSentTransformer.php
@@ -9,16 +9,17 @@ class NotificationSentTransformer extends Transformer
     public function transform(NotificationSent $model)
     {
         $this->output = [
-            'id' => $model->id,
-            'title' => $model->scheduled->title,
-            'message' => $model->scheduled->message,
+            'id'               => $model->id,
+            'title'            => $model->scheduled->title,
+            'message'          => $model->scheduled->message,
             'possible_answers' => $model->scheduled->possible_answers,
-            'sent_at' => $model->sent_at,
-            'read_at' => $model->read_at,
-            'answer' => $model->answer,
-            'answered_at' => $model->answered_at,
+            'sent_at'          => $model->sent_at ? $model->sent_at->toDateTimeString() : null,
+            'read_at'          => $model->read_at ? $model->read_at->toDateTimeString() : null,
+            'answer'           => $model->answer,
+            'answered_at'      => $model->answered_at ? $model->answered_at->toDateTimeString() : null,
         ];
-
+        
         return $this->output;
     }
 }
+//
