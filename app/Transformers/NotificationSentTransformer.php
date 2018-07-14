a8e46793db020e537a9952f278f2166c3d8b667a
Ivan L'olivier <ilolivier@codigodelsur.com>
2018-07-14 15:17:24 -0300

    Removing format of timestamps

--- app/Transformers/NotificationSentTransformer.php
+++ app/Transformers/NotificationSentTransformer.php
@@ -9,14 +9,14 @@ class NotificationSentTransformer extends Transformer
     public function transform(NotificationSent $model)
     {
         $this->output = [
-            'id'               => $model->id,
-            'title'            => $model->scheduled->title,
-            'message'          => $model->scheduled->message,
+            'id' => $model->id,
+            'title' => $model->scheduled->title,
+            'message' => $model->scheduled->message,
             'possible_answers' => $model->scheduled->possible_answers,
-            'sent_at'          => $model->sent_at ? $model->sent_at->diffForHumans() : null,
-            'read_at'          => $model->read_at ? $model->read_at->diffForHumans() : null,
-            'answer'           => $model->answer,
-            'answered_at'      => $model->answered_at ? $model->answered_at->diffForHumans() : null,
+            'sent_at' => $model->sent_at,
+            'read_at' => $model->read_at,
+            'answer' => $model->answer,
+            'answered_at' => $model->answered_at,
         ];
 
         return $this->output;
