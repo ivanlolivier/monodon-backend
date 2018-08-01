fcfe9ca151a8a4fbddb904d9b392eb6a45a5f4ae
Ivan L'olivier <ivan.lolivier@gmail.com>
2018-08-01 11:37:11 -0300

    Adjusting time in appointments

--- app/Transformers/AppointmentTransformer.php
+++ app/Transformers/AppointmentTransformer.php
@@ -16,7 +16,7 @@ class AppointmentTransformer extends Transformer
             'title'       => $model->title,
             'description' => $model->description,
             'date'        => $model->datetime->toDateString(),
-            'time'        => $model->datetime->format('h:i'),
+            'time'        => $model->datetime->format('H:i'),
             'duration'    => $model->duration,
             'clinic_id'   => $model->clinic_id,
             'dentist_id'  => $model->dentist_id,
