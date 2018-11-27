dc81540222bcc3510e760cb0ed88557f655a0e2a
Ivan L'olivier <ivan.lolivier@gmail.com>
2018-11-27 13:20:11 -0300

    Endpoint to mark information as read

--- app/Models/PatientInformation.php
+++ app/Models/PatientInformation.php
@@ -8,6 +8,12 @@ class PatientInformation extends _Model
 {
     protected $fillable = ['information'];
 
+    protected $dates = [
+        'read_at',
+        'created_at',
+        'updated_at',
+    ];
+
     public function patient()
     {
         return $this->belongsTo(Patient::class);
